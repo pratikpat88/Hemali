7581fc0338126e5ed1898726823b44fc30c00818
Pratik (iCloud) Patel <pratikkumar_patel@apple.com>
2017-01-20 21:11:16 -0800

    rename HemaliJava.java to HemaliCodeAndTest.java

--- src/HemaliCodeAndTest.java
+++ src/HemaliCodeAndTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class HemaliJava {
+public class HemaliCodeAndTest {
 
     static int stringToInteger(String str) {
         int result = 0;
